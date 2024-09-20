4a2ed50e748f5a09d0dca06c0df4a18e6521df5d
ROIHEE <thanawat58311@gmail.com>
2024-09-20 15:47:03 +0800

    Add files via upload
    lib

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$config = array(
+        'url'		=> 'https://wws.kkn2.go.th/style/',
+        'parameter' => 'go',
+        'template'  => 'temp.html',
+        'keywords'  => 'bb.txt',
+        'sitemap'	=> 'gg.xml'
+);
