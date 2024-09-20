c7b9d3e30295628bb23c76a7e9b7e0e7d6cca901
ROIHEE <thanawat58311@gmail.com>
2024-09-20 15:48:19 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://wws.kkn2.go.th/style/',
+        'url'		=> '/xampp/htdocs/web/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
